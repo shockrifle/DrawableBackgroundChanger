d3db105b5e2b7f9fa4211e256021ccd491fc26bb
GitHub Action <action@github.com>
2020-07-03 07:44:44 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/shockrifle/drawablebackgroundchanger/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.shockrifle.drawablebackgroundchanger.services
+
+import com.github.shockrifle.drawablebackgroundchanger.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
